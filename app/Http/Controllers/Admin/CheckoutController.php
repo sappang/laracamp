309f437591755c5ccca8facf6cb01678cad2a080
sappang <dsappang@gmail.com>
2022-03-04 15:21:11 +0700

    create email paid

--- app/Http/Controllers/Admin/CheckoutController.php
+++ app/Http/Controllers/Admin/CheckoutController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Checkout;
+use Mail;
+Use App\Mail\Checkout\Paid;
 
 class CheckoutController extends Controller
 {
@@ -13,6 +15,10 @@ class CheckoutController extends Controller
         
         $checkout->is_paid = true;
         $checkout->save();
+
+
+        // send Email
+        Mail::to($checkout->User->email)->send(new Paid($checkout));
 
         $request->session()->flash('success', "Checkout With ID {$checkout->id} has been updated!");
         return redirect(route('admin.dashboard'));
