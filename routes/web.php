a2d6511c132cb2b87d07984d6e2d34bec815d8b6
sappang <dsappang@gmail.com>
2022-03-18 11:26:58 +0700

    MIdtrans Calback Function

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,10 @@ Route::get('sign-in-google', [UserController::class,'google'])->name('user.login
 
 Route::get('auth/google/callback',[UserController::class,'handleProviderCallback'])->name('user.google.callback');
 
+// Midtrans Route
+Route::get('payment/success', [UserController::class,'midtransCallback']);
+Route::post('payment/success', [UserController::class,'midtransCallback']);
+
 Route::middleware(['auth'])->group(function () {
     Route::get('checkout/success',[CheckoutController::class,'success'])->name('checkout.success')->middleware('ensureUserRole:user');
     Route::get('checkout/{camp:slug}',[CheckoutController::class,'create'])->name('checkout.create')->middleware('ensureUserRole:user');
